7c847103cf28638d85b27a365d2d08bb70ff0d7e
Ahmad Hafizh <ahmad.hafizhh@gmail.com>
2024-10-19 15:31:10 +0700

    initiate dashboard

---
+++ layout/dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div id="layout" class="md:mx-auto my-6 mx-auto flex">
+            <div class="shrink-0 w-[5%]"></div>
+            <div class="container ml-8">
+                <slot />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+#layout {
+    height: 85vh;
+}
+</style>
